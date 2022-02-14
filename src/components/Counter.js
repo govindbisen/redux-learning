58a0e38203cdcfc084b875beebc2967715dece31
unknown <govind.bisen@gmail.com>
2022-02-14 15:45:13 +0530

    redux-toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,8 @@
 
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/index";
 // connect for class component
 //we could use usestore
 
@@ -12,17 +14,17 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const incrementHandlerFive = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggle());
   };
 
   return (
