82555d706fb3c61c7617b8be6da7aa61e41273ac
unknown <govind.bisen@gmail.com>
2022-02-14 12:35:31 +0530

    counter with class included in

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,58 @@
+/*
+import { Component } from "react";
+import { connect } from "react-redux";
+import classes from "./Counter.module.css";
+
+class Counter extends Component {
+  // constructor() {
+  //   //state
+  // }
+
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <>
+        <main className={classes.counter}>
+          <h1>Redux Counter</h1>
+          <div className={classes.value}>{this.props.counter}</div>
+          <div>
+            <button onClick={this.incrementHandler.bind(this)}>+</button>
+            <button onClick={this.decrementHandler.bind(this)}>-</button>
+          </div>
+          <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+        </main>
+      </>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+
+*/
+
+// function based component
+
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 // connect for class component
@@ -14,6 +69,9 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+  const incrementHandlerFive = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -24,8 +82,8 @@ const Counter = () => {
       <div>
         <button onClick={incrementHandler}>+</button>
         <button onClick={decrementHandler}>-</button>
+        <button onClick={incrementHandlerFive}>+5</button>
       </div>
-
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
