1212ebf176d06476aeb1243bf38ec6d14a70c81a
unknown <govind.bisen@gmail.com>
2022-02-18 16:07:42 +0530

    fetch put logic inside component app...

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
-const ProductItem = (props) => {
+function ProductItem(props) {
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
@@ -31,6 +31,6 @@ const ProductItem = (props) => {
       </Card>
     </li>
   );
-};
+}
 
 export default ProductItem;
